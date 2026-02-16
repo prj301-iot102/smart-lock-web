bf3b06e09a722ad3ef85a395954fa5b635b92723
TCO46 <patohru2006@gmail.com>
2026-02-16 20:08:54 +0700

    db: add result ENUM for enroll log

--- backend/internal/database/schema/003_nfc_tags.sql
+++ backend/internal/database/schema/003_nfc_tags.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE nfc_tags (
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     uid text NOT NULL,
-	employee_id uuid NOT NULL REFERENCES employees(id),
+    employee_id uuid NOT NULL REFERENCES employees(id),
     is_active boolean NOT NULL,
     enrolled_by uuid NOT NULL REFERENCES users(id),
     created_at timestamptz NOT NULL DEFAULT now()
--- backend/internal/database/schema/005_enrollment_logs.sql
+++ backend/internal/database/schema/005_enrollment_logs.sql
@@ -1,13 +1,15 @@
 -- +goose Up
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TYPE action AS ENUM ('enrolled', 'updated', 'revoked');
+CREATE TYPE action AS ENUM ('enroll', 'update', 'revok');
+CREATE TYPE result AS ENUM ('accepted', 'rejected', 'existed')
 
 CREATE TABLE enrollment_logs (
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     employee_id uuid NOT NULL REFERENCES employees(id),
 	nfc_tag_uid text NOT NULL,
     action status NOT NULL,
+	result result NOT NULL,
     admin_id uuid NOT NULL REFERENCES users(id),
     timestamp timestamptz NOT NULL DEFAULT now()
 );
