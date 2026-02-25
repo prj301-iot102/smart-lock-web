dc7dc6631fc334fbfbc16ff72e646d08120bf69b
TCO46 <patohru2006@gmail.com>
2026-02-25 16:06:29 +0700

    database: fix query and select id for users

--- backend/internal/database/queries/access_logs.sql
+++ backend/internal/database/queries/access_logs.sql
@@ -4,8 +4,8 @@ VALUES(@employee_id, @status)
 RETURNING id;
 
 -- name: GetAccessLogs :many
-SELECT al.id, e.full_name, nt.uid, al.status, al.timestamp
+SELECT al.id, e.full_name, nt.uid, al.status, al.created_at
 FROM access_logs al
 JOIN employees e ON e.id = al.employee_id
 JOIN nfc_tags nt ON nt.id = al.nfc_tag_id
-ORDER BY al.timestamp DESC;
+ORDER BY al.created_at DESC;
--- backend/internal/database/queries/enrollment_logs.sql
+++ backend/internal/database/queries/enrollment_logs.sql
@@ -4,8 +4,8 @@ VALUES (@employee_id, @nfc_tag_uid, @action, @admin_id)
 RETURNING id;
 
 -- name: GetEnrollmentLogs :many
-SELECT el.id, e.full_name, el.nfc_tag_uid, el.action, el.timestamp
+SELECT el.id, e.full_name, el.nfc_tag_uid, el.action, el.created_at
 FROM enrollment_logs el
 JOIN employees e ON e.id = el.employee_id
 JOIN users u ON u.id = el.employee_id
-ORDER BY el.timestamp DESC;
+ORDER BY el.created_at DESC;
