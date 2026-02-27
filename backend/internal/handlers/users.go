6519b91e019e5092f1a316de85adc9f0c5900ed2
patohru <patohru2006@gmail.com>
2026-02-27 21:43:59 +0700

    feat: add get user by id route

---
+++ backend/internal/handlers/users.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/go-fuego/fuego"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prj301-iot102/smart-lock-web/backend/internal/database"
+)
+
+type UsersResource struct {
+	db *pgxpool.Pool
+}
+
+func (ur *UsersResource) GetUser(c fuego.ContextNoBody) (database.GetAccountByIdRow, error) {
+	user_id, err := uuid.Parse(c.PathParam("id"))
+	if err != nil {
+		return database.GetAccountByIdRow{}, fuego.BadRequestError{
+			Detail: "Wrong uuid format",
+			Err:    err,
+		}
+	}
+
+	queries := database.New(ur.db)
+	ctx := context.Background()
+
+	user, err := queries.GetAccountById(ctx, user_id)
+	if err != nil {
+		return database.GetAccountByIdRow{}, fuego.NotFoundError{
+			Detail: "User id not exist",
+			Err:    err,
+		}
+	}
+
+	return user, nil
+}
+
+func UsersRoutes(s *fuego.Server, db *pgxpool.Pool) {
+	rs := UsersResource{
+		db: db,
+	}
+
+	group := fuego.Group(s, "/api/users")
+	fuego.Post(group, "/{id}", rs.GetUser)
+}
