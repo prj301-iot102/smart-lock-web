954c4bcd01bd05cf10b1cccb1dc682afbc6e8038
TCO46 <patohru2006@gmail.com>
2026-02-28 13:15:49 +0700

    database: add UNIQUE for role_name

--- backend/internal/database/schema/001_roles.sql
+++ backend/internal/database/schema/001_roles.sql
@@ -4,7 +4,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE roles (
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     role_name text NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now()
+    created_at timestamptz NOT NULL DEFAULT now(),
+
+	UNIQUE (role_name)
 );
 
 -- +goose Down
