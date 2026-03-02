1e44be5f1f05e31a2758567ab89e69acdb416df3
TCO46 <patohru2006@gmail.com>
2026-03-02 20:24:51 +0700

    database: rearrange migration order

--- backend/internal/database/schema/007_nfc_tags.sql
+++ backend/internal/database/schema/007_nfc_tags.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pg_cron WITH SCHEMA extensions;
 
 CREATE TABLE nfc_tags (
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     uid text NOT NULL,
-    employee_id uuid NOT NULL REFERENCES employees(id),
+    employee_id uuid REFERENCES employees(id),
     is_active boolean NOT NULL,
-    enrolled_by uuid NOT NULL REFERENCES users(id),
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now()
 );
