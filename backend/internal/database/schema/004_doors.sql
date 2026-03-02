dbae58028a6f1086372d29755ba4145425cbb581
TCO46 <patohru2006@gmail.com>
2026-03-02 20:24:19 +0700

    feat: add device_id to door for access permissons

--- backend/internal/database/schema/004_doors.sql
+++ backend/internal/database/schema/004_doors.sql
@@ -4,6 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE doors (
     id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     door_name text NOT NULL,
+    device_id uuid REFERENCES devices(id),
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now()
 );
