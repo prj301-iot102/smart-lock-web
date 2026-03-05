2a8a02f50116607769f24443e2a40ce8308538eb
patohru <patohru2006@gmail.com>
2026-03-05 14:18:37 +0700

    databae: queries for devices and delete enrollment_logs

--- backend/internal/database/queries/devices.sql
+++ backend/internal/database/queries/devices.sql
@@ -2,6 +2,10 @@
 SELECT * FROM devices
 WHERE id = @id;
 
+-- name: GetDeviceByMac :one
+SELECT * FROM devices
+WHERE mac_address = @mac_address;
+
 -- name: UpdateDeviceCanCreate :one
 UPDATE devices
 SET
