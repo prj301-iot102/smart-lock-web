531fb6d431fa8cb4f2e49c9e3e16c161417daf0f
patohru <patohru2006@gmail.com>
2026-03-09 20:04:51 +0700

    fix: missing syntax

--- backend/internal/database/queries/users.sql
+++ backend/internal/database/queries/users.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
 INSERT INTO users(username, password, employee_id)
-VALUES (@username, @password, employee_id)
+VALUES (@username, @password, @employee_id)
 RETURNING id;
 
 -- name: GetAccountByUsername :one
