747169d5c3f3b1a2ccda27682ad83aab4569b432
thaovann09 <thaovann1405@gmail.com>
2026-03-12 23:07:42 +0700

    Add listNfcTags and modified nfc page

--- frontend/js/nfc.js
+++ frontend/js/nfc.js
@@ -1,14 +1,81 @@
-let currentNfcID = null;
-
-async function getNfcByID() {
+async function getNFC() {
+    const id = document.getElementById("nfcID").value;
     const token = localStorage.getItem("token");
-    const id = document.getElementById("nfcID").value.trim();
-    if(!id) {
-        alert("Please enter NFC ID");
+    try{
+        const response = await fetch(`https://smart-lock.patohru.qzz.io/api/nfc/${id}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json, application/xml",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const data = await response.json();
+
+        console.log(data);
+
+        if(response.ok){
+            const table = document.getElementById("nfcTableBody");
+            table.innerHTML = `
+                <tr>
+                    <td>${data.id}</td>
+                    <td>${data.uid}</td>
+                    <td>${data.employee_id}</td>
+                    <td>${data.full_name}</td>
+                    <td>${data.role_name}</td>
+                    <td>${data.is_active}</td>
+                    <td>${data.created_at}</td>
+                    <td>${data.updated_at}</td>
+                    <td>
+                       <button onclick="revokeNFC('${data.id}')">Revoke</button>
+                    </td>
+                </tr>
+            `;
+        } else {
+            document.getElementById("error-msg").innerText = data.message || "NFC not found";
+        }
+    } catch(error) {
+        console.log("Cannot connect to server")
+    }
+}
+
+async function revokeNFC(id) {
+    const token = localStorage.getItem("token");
+    if(!confirm("Are you sure to revoke this NFC tag?")) {
         return;
     }
-    try{
-        const response = await fetch(`https://smart-lock.patohru.qzz.io/api/nfc/${id}`, 
+    try {
+        const response = await fetch(`https://smart-lock.patohru.qzz.io/api/nfc/${id}/revoke`, {
+            method: "PATCH",
+            headers: {
+                "Accept": "application/json, application/xml",
+                "Authorization": `Bearer ${token}`
+            }
+        }
+        );
+
+        const data = await response.json();
+
+        if(response.ok) {
+            alert("NFC revoked successfully");
+            getNFC();
+        } else {
+            alert(data.message || "Rovoke NFC failed");
+        }
+    } catch(error) {
+        console.log("Error: ", error)
+    }
+}
+
+async function listNfcTags() {
+    const token = localStorage.getItem("token");
+    const table = document.getElementById("nfcTableBody");
+    const nfcMessage = document.getElementById("error-msg");
+
+    table.innerHTML = "";
+    nfcMessage.textContent = "";
+
+    try {
+        const response = await fetch("https://smart-lock.patohru.qzz.io/api/nfc/",
             {
                 method: "GET",
                 headers: {
@@ -18,57 +85,37 @@ async function getNfcByID() {
             }
         );
         if(!response.ok) {
-            throw new Error("NFC not found");
+            nfcMessage.textContent = "Failed to lead NFC tags";
+            return;
         }
-        const nfc = await response.json();
-        renderNFCInfo(nfc);
-        
-    } catch(error) {
-        console.log("Error getting NFC: ", error);
-    }
-}
-
-function renderNFCInfo(nfc) {
-
-    currentNfcID = nfc.id;
-
-    document.getElementById("nfcID").textContent = nfc.id;
-    document.getElementById("nfcUID").textContent = nfc.uid;
-    document.getElementById("employeeID").textContent = nfc.employee_id;
-    document.getElementById("fullName").textContent = nfc.full_name;
-    document.getElementById("username").textContent = nfc.username;
-    document.getElementById("status").textContent = nfc.is_active ? "ACTIVE" : "REVOKED";
-    document.getElementById("createdAt").textContent = nfc.created_at;
-    document.getElementById("updatedAt").textContent = nfc.updated_at;
-}
-
-async function revokeNFC() {
-    const token = localStorage.getItem("token");
-    if(!currentNfcID) {
-        alert("No NFC selected");
-        return;
-    }
-    if(!confirm("Are you sure to revoke this NFC tag?")) {
-        return;
-    }
-    try {
-        const response = await fetch(`https://smart-lock.patohru.qzz.io/api/nfc/${currentNfcID}/revoke`, 
-            {
-                method: "PATCH",
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Accept": "application/json, application/xml",
-                    "Content-Type": "application/json"
-                }
-            }
-        );
-        if(!response.ok) {
-            throw new Error("Revoke NFC failed");
+        const nfcTags = await response.json();
+        if(!nfcTags.data || nfcTags.data.length === 0) {
+            nfcMessage.textContent = "No NFC tags found";
+            return;
         }
-        alert("NFC revoked");
-        getNfcByID();    
+
+        nfcTags.data.forEach(nfc => {
+            const tableRow = document.createElement("tr");
+            tableRow.innerHTML = `
+                <td>${nfc.id}</td>
+                <td>${nfc.uid}</td>
+                <td>${nfc.employee_id}</td>
+                <td>${nfc.full_name}</td>
+                <td>${nfc.role_name}</td>
+                <td>${nfc.is_active}</td>
+                <td>${nfc.created_at}</td>
+                <td>${nfc.updated_at}</td>
+                <td>
+                    <button onclick="revokeNFC('${nfc.id}')">Revoke</button>
+                </td>
+            `;
+            table.appendChild(tableRow);
+        });
     } catch(error) {
-        console.log("Error: ", error)
+        console.log("Error listing NFC Tags: ", error);
     }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    listNfcTags();
+});
