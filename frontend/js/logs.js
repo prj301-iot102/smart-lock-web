e2162895bfd0670910a1cccd3472ade69e7e624e
WhiteDev <thanh2982006@gmail.com>
2026-03-12 15:06:23 +0700

    add logs css config and add js for sidebar

--- frontend/js/logs.js
+++ frontend/js/logs.js
@@ -4,7 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function listLogs() {
     const table = document.getElementById("logsTableBody");
-
+    const totaltable = document.getElementById("totalAccess");
+    const totalgranted = document.getElementById("granted");
+    const totaldenied = document.getElementById("denied")
+    let total = 0;
+    let granted = 0;
+    let denied = 0;
     try {
         const response = await fetch("https://smart-lock.patohru.qzz.io/api/accessLog/", 
             {
@@ -16,17 +21,32 @@ async function listLogs() {
         );
         const data = await response.json();
         table.innerHTML = "";
+        totaldenied.innerHTML = "";
+        totalgranted.innerHTML = "";
+        totaltable.innerHTML = "";
+
         data.forEach(log => {
             const tableRow = document.createElement("tr");
             tableRow.innerHTML = `
-                <td>${log.id}</td>
                 <td>${log.created_at}</td>
                 <td>${log.full_name}</td>
                 <td>${log.uid}</td>
                 <td>${log.status}</td>
             `;
+            total = total + 1
             table.appendChild(tableRow);
+            if (log.status === "granted") {
+                tableRow.classList.add("granted");
+                granted++
+            } else {
+                tableRow.classList.add("denied");
+                denied++
+            }
         });
+        totaldenied.innerHTML = denied;
+        totalgranted.innerHTML = granted;
+        totaltable.innerHTML = total;
+
     } catch(error) {
         console.log("Error loading logs: ", error);
     }
